3806af9db617715a90a5a138d2baa1f5b9f0305b
aorandomcrew <danbogomolov@gmail.com>
2023-04-19 02:21:08 +0300

    исправил по полной

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,25 @@ public class Main {
 
     public static void main(String[] args) {
         //Задание 1
+        System.out.println("задание 1");
         int year = 2020;
         checkIfYearLeap(year);
         System.out.println();
         //Задание 2
+        System.out.println("задание 2");
         int clientOs = 0;
         int clientDeviceYear = 2016;
         checkClientOS(clientOs, clientDeviceYear);
         System.out.println();
         //Задание 3
-        int deliveryDistance = 95;
+        System.out.println("задание 3");
+        int deliveryDistance = 110;
         int days = 0;
-        checkDeliveryDistance(deliveryDistance, days);
+        days = checkDeliveryDistance(deliveryDistance,days);
+        System.out.println("Потребуется дней: " + days);
     }
 
     public static void checkIfYearLeap(int year) {
-        System.out.println("задание 1");
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println("год " + year + " является високосным");
         } else {
@@ -26,7 +29,6 @@ public class Main {
     }
 
     public static void checkClientOS(int clientOS, int clientDeviceYear) {
-        System.out.println("задание 2");
         if (clientDeviceYear < 2015) {
             switch (clientOS) {
                 case 0:
@@ -48,21 +50,17 @@ public class Main {
     }
 
     public static int checkDeliveryDistance(int deliveryDistance, int days) {
-        System.out.println("Задание 3");
         if (deliveryDistance < 0 || deliveryDistance >= 100) {
             System.out.println("Доставка не осуществляется");
         }
         if (deliveryDistance <= 20) {
             days = days + 1;
-            System.out.println("Потребуется дней: " + days);
         }
         if (deliveryDistance > 20 && deliveryDistance < 60) {
             days = days + 2;
-            System.out.println("Потребуется дней: " + days);
         }
         if (deliveryDistance > 60 && deliveryDistance < 100) {
             days = days + 3;
-            System.out.println("Потребуется дней: " + days);
         }
         return days;
     }
